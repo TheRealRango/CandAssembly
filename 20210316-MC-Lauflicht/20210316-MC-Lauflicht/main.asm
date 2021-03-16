00fd79501868cd926d00902e77cfff31a63223ae
Lorenz <lorenzfaber@gmail.com>
2021-03-16 15:11:48 +0100

    added lauflicht task

---
+++ 20210316-MC-Lauflicht/20210316-MC-Lauflicht/main.asm
@@ -0,0 +1,29 @@
+;
+; 20210316-MC-Lauflicht.asm
+;
+; Created: 16/03/2021 14:44:26
+; Author : lorenz
+;
+
+start:
+    ldi R24, 0xff
+    out DDRA, R24
+    ldi R16, 0b00000001
+    ldi R17, 0b00000010
+    ldi R18, 0b00000100
+    ldi R19, 0b00001000
+    ldi R20, 0b00010000 
+    ldi R21, 0b00100000
+    ldi R22, 0b01000000
+    ldi R23, 0b10000000 
+loop:
+    out PORTA, R16
+    out PORTA, R17
+    out PORTA, R18
+    out PORTA, R19
+    out PORTA, R20
+    out PORTA, R21
+    out PORTA, R22
+    out PORTA, R23
+    
+    jmp loop
