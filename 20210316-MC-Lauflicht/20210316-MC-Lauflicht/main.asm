c4fafaaa9f08e686d784710eeacc06db9c8249ec
Lorenz <lorenzfaber@gmail.com>
2021-03-19 09:45:10 +0100

    updated main asm

--- 20210316-MC-Lauflicht/20210316-MC-Lauflicht/main.asm
+++ 20210316-MC-Lauflicht/20210316-MC-Lauflicht/main.asm
@@ -27,3 +27,21 @@ loop:
     out PORTA, R23
     
     jmp loop
+
+	
+    
+    
+    
+      start:
+    ldi R24, 0xff
+    out DDRA, R24
+init:
+    ldi R16, 0b00000001
+    
+loop:
+    cpi R16, 0b00000000
+    breq init
+    out PORTA, R16
+    add R16, R16
+    
+    jmp loop
