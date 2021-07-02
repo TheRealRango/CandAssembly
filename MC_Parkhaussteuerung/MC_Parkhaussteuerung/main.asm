c485b018ce81336a88e5be67f3cac91c48bdf90f
Lorenz <lorenzfaber@gmail.com>
2021-07-02 14:50:35 +0200

    added the parkhausproject

---
+++ MC_Parkhaussteuerung/MC_Parkhaussteuerung/main.asm
@@ -0,0 +1,22 @@
+;
+; MC_Parkhaussteuerung.asm
+;
+; Created: 02/07/2021 13:55:50
+; Author : lorenz
+;
+
+
+; Replace with your application code
+PullUp:
+	ldi r16, 0xFF
+	out DDRA, r16 ;als Ausgang definieren
+	ldi r17, 0x00
+	out DDRD r17 ;als Eingang definieren
+init:
+	ldi r18, 0b00000000
+	out PORTA,r18  ;alle ausgaenge auf 0 setzen für Betriebsbeginn
+
+start:
+	
+
+
