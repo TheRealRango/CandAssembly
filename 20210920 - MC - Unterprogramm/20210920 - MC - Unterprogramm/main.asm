c8f1f1e33adfe146966dcabcaf42067e318a4bcd
Lorenz <lorenzfaber@gmail.com>
2021-09-20 09:47:28 +0200

    updated unterprogramme

--- 20210920 - MC - Unterprogramm/20210920 - MC - Unterprogramm/main.asm
+++ 20210920 - MC - Unterprogramm/20210920 - MC - Unterprogramm/main.asm
@@ -5,6 +5,13 @@
 ; Author : lorenz
 ;
 
+ldi r17, high(RAMEND)	;stackpointer initialisieren
+out sph, r17			;HIGH-Byte der obersten RAMAdresse
+ldi r18, low(RAMEND)	;low byte der obersten ramadresse
+out spl, r18
+
+push R17
+push R18
 
 ; Replace with your application code
 start:
