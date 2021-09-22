6b2edf357b0c7f9e0debfd52c8cc044e285f8e44
Lorenz <lorenzfaber@gmail.com>
2021-09-22 12:16:38 +0200

    added Lauflicht Project in C

---
+++ 20210922 - MC - LaufLichtC/20210922 - MC - LaufLichtC/main.c
@@ -0,0 +1,21 @@
+/*
+ * 20210922 - MC - LaufLichtC.c
+ *
+ * Created: 22/09/2021 12:12:30
+ * Author : lorenz
+ */ 
+
+#include <avr/io.h>		//include = einbinden von 
+						//io = input/output-Bib --> wird für Bezeichnungen benötigt
+						//damit lesbare Befehle eingegeben werden können
+						
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
