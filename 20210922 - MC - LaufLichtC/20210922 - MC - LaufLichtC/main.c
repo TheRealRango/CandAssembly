fff6a5ba2072942ad1317b96c09339e13aee5f72
Lorenz <lorenzfaber@gmail.com>
2021-09-22 12:47:33 +0200

    updated C project lauflicht

--- 20210922 - MC - LaufLichtC/20210922 - MC - LaufLichtC/main.c
+++ 20210922 - MC - LaufLichtC/20210922 - MC - LaufLichtC/main.c
@@ -1,21 +1,28 @@
 /*
- * 20210922 - MC - LaufLichtC.c
- *
- * Created: 22/09/2021 12:12:30
- * Author : lorenz
- */ 
+* 20210922 - MC - LaufLichtC.c
+*
+* Created: 22/09/2021 12:12:30
+* Author : lorenz
+*/
 
-#include <avr/io.h>		//include = einbinden von 
+
+
+
+#include <avr/io.h>		//include = einbinden von
 						//io = input/output-Bib --> wird f¸r Bezeichnungen benˆtigt
 						//damit lesbare Befehle eingegeben werden kˆnnen
-						
 
+int variable_a = 30;	//deklaration und initialisierung von variable_a
 
-int main(void)
+int main(void)			//Hauptfunktion -> hier wird als erstes gesprungen
+						//main -> Name der Funktion
+						//r¸ckgabe wert ist int
+						//void --> erwartet keine ¸bergabe Parameter
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	/* Replace with your application code */
+	while (1)			//eins ist boolean und steht f¸r true Endlosschleife --> weil die Abbruchbedingung nie erreicht wird.
+						//alle werte sind auﬂer 0, false wahr!
+	{
+	}
 }
 
