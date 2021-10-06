57bb498644728b75b3d66aafed4d3c6977837a45
Lorenz <lorenzfaber@gmail.com>
2021-10-06 12:54:22 +0200

    schrittmotor 2.0

--- 20211006-MC-Schrank/20211006-MC-Schrank/main.c
+++ 20211006-MC-Schrank/20211006-MC-Schrank/main.c
@@ -1,34 +1,34 @@
 /*
- * 20211006-MC-Schrank.c
- *
- * Created: 06/10/2021 12:48:45
- * Author : lorenz
- */ 
+* 20211006-MC-Schrank.c
+*
+* Created: 06/10/2021 12:48:45
+* Author : lorenz
+*/
 
 #include <avr/io.h>
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    DDRA = 0xFF;
-    PORTA = 0x00;
-    unsigned int tab[8] = {0b1010,0b1000,0b1001,0b0001,0b0101,0b0100,0b0110,0b0010};
-    
-    
-    while (1)
-    {
-	    int i = 0;
-	    
-	    while (i<8)
-	    {
-		    PORTA = tab[i];
-		    _delay_ms(500);
-		    i++;
-	    }
-	    
-	    
-	    
-    }
+	/* Replace with your application code */
+	DDRA = 0xFF;
+	PORTA = 0x00;
+	PORTA = 0b1010;
+	//unsigned int tab[4] = {0b1010,0b1001,0b0101,0b0110};
+
+
+
+
+	while (1)
+	{
+		//int i = 0;
+		PORTA = 0b1010;
+		/*while (i<4)
+		{
+			PORTA = tab[i];
+			_delay_ms(2000);
+			i++;
+		}*/
+	}
 }
 
